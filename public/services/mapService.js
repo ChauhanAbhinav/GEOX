a2cca27f613772f87bafa857e2eb23d384b79254
Abhinav Chauhan <abhinavchauhan25493@gmail.com>
2018-08-17 09:16:58 +0530

    dashboard updated

--- public/services/mapService.js
+++ public/services/mapService.js
@@ -5,50 +5,52 @@ angular.module('mapService',[])
     this.init = function(cent) {
         var options = {
             center: cent,
-            zoom: 19,
+            zoom: 17,
+            mapTypeControl: false,
+            streetViewControl: false,
             disableDefaultUI: false    
         }
         this.map = new google.maps.Map(
             document.getElementById("map"), options
         );
-       
+
     }
       this.addMarkers = function(pos) {
-        //if(this.marker) this.marker.setMap(null); // this will remove previous marker
-        
         this.userMarker = new google.maps.Marker({
             map: this.map,
             position: pos,
-            icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+            //icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
             title: 'Your location',
             animation: google.maps.Animation.DROP
         });
-    //     var infowindow = new google.maps.InfoWindow({
-    // 	content: "<span style='font-size:12px'>Your Location</span>"
-  		// });
-    //     infowindow.open(this.map, this.userMarker)
-        }
+    }
        this.addFence = function(){
-       	var tl = new google.maps.LatLng(26.788336, 75.827062);
-  		var tr = new google.maps.LatLng(26.788336, 75.827437);
-  		var br = new google.maps.LatLng(26.788134, 75.827437);
-  		var bl = new google.maps.LatLng(26.788134, 75.827062);
+    //    	var tl = new google.maps.LatLng(26.788336, 75.827062);
+  		// var tr = new google.maps.LatLng(26.788336, 75.827437);
+  		// var br = new google.maps.LatLng(26.788134, 75.827437);
+  		// var bl = new google.maps.LatLng(26.788134, 75.827062);
+       //  testing coordinates
+      var tl = new google.maps.LatLng(26.805854, 75.798464);
+      var tr = new google.maps.LatLng(26.805854, 75.799156);
+      var br = new google.maps.LatLng(26.805413, 75.799156);
+      var bl = new google.maps.LatLng(26.805413, 75.798464);
 
   		var cdac = new google.maps.Polygon({
     	path: [tl, tr, br, bl],
     	strokeColor: "#ff0000",
-   		strokeOpacity: 0.8,
+   		strokeOpacity: 0.7,
     	strokeWeight: 2,
     	fillColor: "#ff0000",
-    	fillOpacity: 0.4,
+    	fillOpacity: 0.3,
     	title: 'cdac'
   		});
   		cdac.setMap(this.map);
   		var infowindowCdac = new google.maps.InfoWindow({
     	content: "<span style='font-size:12px'>Reach here for <br>attendance</span>",
-    	position: {lat: 26.788298, lng:  75.827143}
+       position: {lat: 26.788298, lng:  75.827143}
+      // position: {lat: 26.805600, lng: 75.799250}
   		});
-        infowindowCdac.open(this.map)
+        // infowindowCdac.open(this.map)
         }
     
 });
