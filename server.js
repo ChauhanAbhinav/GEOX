d4b867c2a7ac9ca7c96ecd7c3b07662dcfb278db
Abhinav Chauhan <abhinavchauhan25493@gmail.com>
2018-08-17 09:17:13 +0530

    serverjs updated

--- server.js
+++ server.js
@@ -1,29 +1,25 @@
-var http = require('http');
-var https = require('https');
 var express = require('express');
 var bodyParser=require('body-parser');
+var session = require('express-session');
 var app=express();
 var path=require('path');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.use(express.static(__dirname+"/public"));
-//reading ssl certificate for https
-// var fs = require("fs");
-// const options = {
-//   key: fs.readFileSync("./cert/44737450_localhost.key"),
-//   cert: fs.readFileSync("./cert/44737450_localhost.cert")
-// };
-
-// load up login page
-app.get('/', function(req, res) {
-    res.sendFile(__dirname+'/public/views/login.html');
-    console.log("login page loaded"); 
-	});
+app.use(session({
+  secret: 'geox',
+  resave: true,
+  saveUninitialized: false,
+  //cookie: { secure: true }
+}));
 
 //load modules
+var mainRoutes = require("./app/routes/mainRoutes.js")
 var loginRoutes = require("./app/routes/loginRoutes.js");
 var signupRoutes = require("./app/routes/signupRoutes.js");
+
+mainRoutes(app,path);
 
 //connection to mongoDB
 var mongo=require('mongodb');
@@ -31,9 +27,8 @@ var url="mongodb://localhost:27017";
 mongo.connect(url,function(err, db){
 		if(err) throw err;
 		signupRoutes(app,db,path);
-		loginRoutes(app,db);
+		loginRoutes(app,db,path);
 		console.log("connected to mongodb");
 });
-//https.createServer(options, app).listen(8080); //for secure connection https
-app.listen(8080); // for non secure connection
+app.listen(8080);
 console.log("app is listening on port 8080");
